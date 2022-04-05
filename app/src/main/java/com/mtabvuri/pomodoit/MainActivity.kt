02a4ce9e8dbb1909f45e035d28a47ee0347a3209
AfricanBongo <tilormta@outlook.com>
2022-04-05 08:08:27 +0200

    Updated NavHost to AnimatedNavHost.

--- app/src/main/java/com/mtabvuri/pomodoit/MainActivity.kt
+++ app/src/main/java/com/mtabvuri/pomodoit/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.mtabvuri.pomodoit.nav.PomoDoItNavHost
 import com.mtabvuri.pomodoit.ui.theme.PomoDoItTheme
 
@@ -22,11 +24,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun PomoDoItApp(window: Window?) {
     PomoDoItTheme {
         window?.statusBarColor = MaterialTheme.colors.primaryVariant.toArgb()
-        val navController = rememberNavController()
+        val navController = rememberAnimatedNavController()
         PomoDoItNavHost(navController)
     }
 }
--- app/src/main/java/com/mtabvuri/pomodoit/nav/NavHost.kt
+++ app/src/main/java/com/mtabvuri/pomodoit/nav/NavHost.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.pm.ActivityInfo
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -12,7 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.mtabvuri.pomodoit.nav.NavScreen.*
 import com.mtabvuri.pomodoit.ui.home.HomeScreen
 import com.mtabvuri.pomodoit.ui.preferences.PreferencesScreen
@@ -26,18 +29,61 @@ enum class NavScreen {
     Splash, Preferences, Home, Settings
 }
 
+/**
+ * Navigation for the entire app.
+ * @param navController An AnimatedNavHostController.
+ */
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun PomoDoItNavHost(navController: NavHostController) {
-    NavHost(
+    AnimatedNavHost(
         navController = navController, startDestination = Splash.name
     ) {
-        composable(Splash.name) { SplashScreen {
+        composable(route = Splash.name) { SplashScreen {
             navController.navigate(Preferences.name)
         }}
-        composable(Preferences.name) { PreferencesScreen {
-            navController.navigate(Home.name)
-        }}
-        composable(Home.name) { HomeScreen() }
+        composable(
+            Preferences.name,
+            enterTransition = {
+                when (initialState.destination.route) {
+                    Splash.name ->
+                        slideIntoContainer(AnimatedContentScope.SlideDirection.Up)
+                    else -> null
+                }
+            },
+            exitTransition = {
+                when (targetState.destination.route) {
+                    Home.name ->
+                        slideOutOfContainer(AnimatedContentScope.SlideDirection.Left)
+                    else -> null
+                }
+            }
+        ) {
+            PreferencesScreen {
+                navController.navigate(Home.name)
+            }
+        }
+
+        composable(
+            Home.name,
+            enterTransition = {
+                when (initialState.destination.route) {
+                    Preferences.name ->
+                        slideIntoContainer(AnimatedContentScope.SlideDirection.Left)
+                    else -> null
+                }
+            },
+            exitTransition = {
+                when (targetState.destination.route) {
+                    Settings.name ->
+                        slideOutOfContainer(AnimatedContentScope.SlideDirection.Up)
+                    else -> null
+                }
+            }
+        ) {
+            HomeScreen()
+        }
+
         composable(Settings.name) { SettingsScreen() }
     }
 }
