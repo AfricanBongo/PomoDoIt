ad316c529b49e13fab40b482b748b83f715d5cc7
AfricanBongo <tilormta@outlook.com>
2022-04-07 19:22:01 +0200

    Progress pill init

--- app/src/main/java/com/mtabvuri/pomodoit/ui/home/HomeScreen.kt
+++ app/src/main/java/com/mtabvuri/pomodoit/ui/home/HomeScreen.kt
@@ -1,11 +1,26 @@
 package com.mtabvuri.pomodoit.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mtabvuri.pomodoit.R
+import com.mtabvuri.pomodoit.ui.components.PrimaryButton
+import com.mtabvuri.pomodoit.ui.theme.PomoDoItTheme
+import com.mtabvuri.pomodoit.ui.theme.PrimaryMediumEmphasis
+import com.mtabvuri.pomodoit.ui.theme.Progress
 
 @Composable
 fun HomeScreen() {
-
+    ProgressGroup(
+        progress = 0.95f,
+        modifier = Modifier.padding(4.dp)
+    )
 }
 
 /*
@@ -17,12 +32,89 @@ private fun ButtonBox(
     modifier: Modifier = Modifier,
     pomodoroState: PomodoroState
 ) {
-    when (pomodoroState) {
-        // TODO
+
+}
+
+@Composable
+private fun ProgressGroup(
+    progress: Float,
+    modifier: Modifier = Modifier
+) {
+
+//    TODO
+//    val progressPillModifier = Modifier
+//        .clip(MaterialTheme.shapes.small)
+//        .background(PrimaryMediumEmphasis)
+//        .requiredWidth(40.dp)
+//        .requiredHeight(24.dp)
+//
+//    val filledPills: Int = progress.coerceIn(0f, 1f).div(0.25f).toInt()
+//    var individualProgress: Float
+//    Row(
+//        modifier = modifier,
+//        horizontalArrangement = Arrangement.spacedBy(8.dp)
+//    ) {
+//       for (i in 1..filledPills) {
+//           individualProgress = if (filledPills > i) 1f
+//           else (progress % 0.25f) * 4
+//
+//           ProgressPill(progress = individualProgress, modifier = progressPillModifier)
+//       }
+//    }
+}
+
+@Composable
+private fun ProgressPill(
+    progress: Float,
+    modifier: Modifier = Modifier
+) {
+    BoxWithConstraints(
+        modifier = modifier
+    ) {
+        var filledProgress = progress.coerceIn(0f, 1f)
+        filledProgress = (filledProgress / 1f) * maxWidth.value
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxHeight()
+                .requiredWidth(filledProgress.dp)
+                .background(Progress)
+        )
+
     }
 }
 
 @Composable
-private fun StartPomodoroButton() {
+fun StartPomodoroButton(
+    modifier: Modifier = Modifier,
+    pomodoroState: PomodoroState
+) {
+    val stringResourceId = when (pomodoroState) {
+        is PomodoroState.Default -> {
+            if (pomodoroState.completedPomodoros == 4) {
+                R.string.start_another_btn
+            } else {
+                R.string.start_btn
+            }
+        }
+        else -> R.string.start_btn
+    }
 
+    PrimaryButton(
+        modifier = modifier,
+        text = stringResource(stringResourceId),
+        onClick = when (pomodoroState) {
+            is PomodoroState.Running -> pomodoroState.onWait
+            is PomodoroState.Default -> pomodoroState.onStart
+            else -> {return Unit}
+        }
+    )
+}
+
+@Preview(heightDp = 800, widthDp = 320, showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    PomoDoItTheme {
+        HomeScreen()
+    }
 }
--- app/src/main/java/com/mtabvuri/pomodoit/ui/home/PomodoroState.kt
+++ app/src/main/java/com/mtabvuri/pomodoit/ui/home/PomodoroState.kt
@@ -2,11 +2,9 @@ package com.mtabvuri.pomodoit.ui.home
 
 /**
  * A state in the pomodoro session.
- * @param changeState Trigger function when we want to change the state.
  */
 sealed class PomodoroState {
-    object Default: PomodoroState()
-    class Play(val onPause: () -> Unit) : PomodoroState()
-    class Pause(val onResume: () -> Unit, val onStop: () -> Unit) : PomodoroState()
-    class Completed(val completedPomodoros: Int, onStart: () -> Unit): PomodoroState()
+    class Running(val onWait: () -> Unit) : PomodoroState()
+    class Waiting(val onRun: () -> Unit, val onStop: () -> Unit) : PomodoroState()
+    class Default(val completedPomodoros: Int, val onStart: () -> Unit): PomodoroState()
 }
